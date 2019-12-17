4fd407bdffc533777e9400bc28d8e5134b8dbc15
Tomasz Naskręt <naskret.tomasz@gmail.com>
2019-12-17 09:42:53 +0100

    ignore folders

--- geolocation-service/src/main/java/pl/clarin/geocoder/GeolocationService.java
+++ geolocation-service/src/main/java/pl/clarin/geocoder/GeolocationService.java
@@ -30,75 +30,77 @@ public class GeolocationService {
         return instance;
     }
 
-    public void process(Path p,  int limit){
+    public void process(Path p, int limit) {
         AtomicInteger c = new AtomicInteger(0);
-        if(Files.isDirectory(p)){
+        if (Files.isDirectory(p)) {
             try {
-                List<Path> fileWithName = Files.walk(p)
+                List<Path> files = Files.walk(p)
                         .map(Path::toAbsolutePath)
                         .collect(Collectors.toList());
-                for (Path name : fileWithName) {
-                    System.out.println(c.getAndIncrement());
-                   run(name ,limit);
+
+                for (Path name : files) {
+                    LOGGER.log(Level.INFO, "Processing file:" + c.getAndIncrement() + "/" + files.size());
+                    run(name, limit);
                 }
             } catch (IOException e) {
-                LOGGER.log(Level.SEVERE, "Unable to load file",  e);
+                LOGGER.log(Level.SEVERE, "Unable to load file", e);
             }
 
-        } else if (Files.isRegularFile(p)){
+        } else if (Files.isRegularFile(p)) {
             run(p, limit);
         }
         MapsLocationStore.getInstance().save();
     }
 
     private void run(Path path, int limit) {
+        if (!Files.isDirectory(path)) {
+            try (InputStream is = Files.newInputStream(path)) {
+                g419.corpus.structure.Document ccl = AnnotationReader.getInstance().loadDocument(is);
+                if (ccl != null) {
+                    ccl.getParagraphs().forEach(p -> {
+                        p.getSentences().forEach(s -> {
 
-        try (InputStream is = Files.newInputStream(path)) {
-            g419.corpus.structure.Document ccl = AnnotationReader.getInstance().loadDocument(is);
-            if (ccl != null) {
-                ccl.getParagraphs().forEach(p -> {
-                    p.getSentences().forEach(s -> {
+                            Map<Integer, List<g419.corpus.structure.Annotation>> ann = s.getChunks()
+                                    .stream()
+                                    .filter(a ->
+                                            a.getType().startsWith("nam_loc") ||
+                                                    a.getType().startsWith("nam_fac") ||
+                                                    a.getType().startsWith("nam_num"))
+                                    .collect(Collectors.groupingBy(g419.corpus.structure.Annotation::getChannelIdx));
 
-                        Map<Integer, List<g419.corpus.structure.Annotation>> ann = s.getChunks()
-                                .stream()
-                                .filter(a ->
-                                        a.getType().startsWith("nam_loc") ||
-                                        a.getType().startsWith("nam_fac") ||
-                                        a.getType().startsWith("nam_num"))
-                                .collect(Collectors.groupingBy(g419.corpus.structure.Annotation::getChannelIdx));
+                            ann.forEach((k, v) -> {
 
-                        ann.forEach((k, v) -> {
+                                String q = v.stream().map(Annotation::getBaseText)
+                                        .collect(Collectors.joining(" "));
+                                AtomicReference<String> arg = new AtomicReference<>("nam_loc");
+                                v.stream().findFirst().ifPresent(a -> {
+                                    arg.set(a.getType());
+                                });
+                                try {
+                                    List<Location> loc = GeoNamesLocationFinder.getInstance().query(q);
+                                    if (loc.isEmpty()) {
+                                        loc = MapsLocationFinder.getInstance().query(q, limit, "pl");
+                                    }
 
-                            String q = v.stream().map(Annotation::getBaseText)
-                                    .collect(Collectors.joining(" "));
-                            AtomicReference<String> arg = new AtomicReference<>("nam_loc");
-                            v.stream().findFirst().ifPresent(a ->{
-                                      arg.set(a.getType());
+                                    AtomicInteger count = new AtomicInteger(1);
+                                    loc.stream().limit(limit).forEach(l -> {
+                                        s.getAnnotationInChannel(arg.get(), k).getMetadata()
+                                                .put("coord:" + count.get(), l.toPropertyValue());
+                                        count.getAndIncrement();
                                     });
-                            try {
-                                List<Location> loc = GeoNamesLocationFinder.getInstance().query(q);
-                                if(loc.isEmpty()) {
-                                    loc = MapsLocationFinder.getInstance().query(q, limit, "pl");
+                                    count.set(1);
+                                } catch (IOException e) {
+                                    LOGGER.log(Level.SEVERE, "Geo service error", e);
                                 }
 
-                                AtomicInteger count = new AtomicInteger(1);
-                                loc.stream().limit(limit).forEach(l -> {
-                                    s.getAnnotationInChannel(arg.get(), k).getMetadata()
-                                            .put("coord:" + count.get(), l.toPropertyValue());
-                                    count.getAndIncrement();
-                                });
-                                count.set(1);
-                            } catch (IOException e) {
-                                LOGGER.log(Level.SEVERE,"Geo service error", e);
-                            }
-
+                            });
                         });
                     });
-                });
+                }
+                AnnotationWriter.getInstance().writeDocument(ccl, path);
+            } catch (IOException e) {
+                LOGGER.log(Level.SEVERE, "File error", e);
             }
-            AnnotationWriter.getInstance().writeDocument(ccl, path);
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "File error", e);
         }
     }
 }
